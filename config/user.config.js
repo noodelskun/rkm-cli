b780937ca62f8d5b63489555ca8a767dba412533
13926728608 <ruankangmin@qilitech.ltd>
2020-11-13 10:40:03 +0800

    globalModules

--- config/user.config.js
+++ config/user.config.js
@@ -1,8 +1,17 @@
+/*
+ * @Author: rkm
+ * @Date: 2020-11-13 10:15:33
+ * @LastEditTime: 2020-11-13 10:39:41
+ * @FilePath: \rkm-cli\config\user.config.js
+ * @LastEditors: rkm
+ */
 const path = require('path')
 const fs = require('fs')
     /* 获取用户定义配置 */
 async function getUserConfig() {
-    let config = {}
+    let config = {
+        globalModules: []
+    }
     try {
         const res = await fs.readFileSync(path.join(process.cwd(), 'km.config.js'))
         if (res) {
